33751e2a336eb6f490d7e52390ca7b4984af6255
Mathieu <mathieu.echarte1234@gmail.com>
2020-06-05 16:00:12 +0200

    Création du formulaire du tournoi + ajout du bouton

--- src/Form/TournoiType.php
+++ src/Form/TournoiType.php
@@ -2,11 +2,14 @@
 
 namespace App\Form;
 
+use App\Entity\Serie;
 use App\Entity\Tournoi;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 class TournoiType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
@@ -17,9 +20,18 @@ class TournoiType extends AbstractType
             ->add('nbJoueursParEquipe')
             ->add('etat')
             ->add('libelle')
-        ;
+            ->add('series', EntityType::class, 
+            [
+                'class' => Serie::class,
+                'choice_label' => function(Serie $serie)
+                {
+                  return $serie->getLibelle();  
+                },
+                    'multiple' => true,
+                    'expanded' => true
+            ]);
     }
-
+    
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
